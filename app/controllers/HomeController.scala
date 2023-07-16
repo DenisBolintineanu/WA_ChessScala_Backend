eca391e089a5bb02b36ab3ddeff5fabe56dc0822
andre-koe <andre.koeniger1997@gmail.com>
2023-07-16 18:34:32 +0200

    Implement GarbageCollection for GameSessions in CleanUp task, add AdminPage to view all GameSessions including their status, add admin.scala.html

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,19 +1,16 @@
 package controllers
 
-import javax.inject._
-import play.api._
 import play.api.mvc._
-import ChessScala.controller.IController
-import ChessScala.controller.Controller
+import utils.CleanUpTask
 
-import scala.collection.mutable
+import javax.inject._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents, val cleanUpTask: CleanUpTask) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -23,28 +20,28 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
 
-  private var controllerMapping: Map[String, IController] = Map.empty
-
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val gameID = java.util.UUID.randomUUID().toString
-    val controller = new Controller()
-    controllerMapping = controllerMapping + (gameID -> controller)
-    Ok(views.html.index(gameID))
+    Ok(views.html.index(cleanUpTask.gameSetup))
   }
 
   def playChess2(id: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val parameter1 = request.body.asFormUrlEncoded.get("command").headOption.getOrElse("")
-    val controller = controllerMapping(id)
+    val controller = cleanUpTask.controllerMapping(id)
+    cleanUpTask.garbageCollector = cleanUpTask.garbageCollector.map(x => if (x._1 == id) (x._1, true) else x)
     controller.computeInput(parameter1)
     val controllerAsText: String = controller.output
-    Ok(views.html.chess(controllerAsText, id))
+    Ok(views.html.chess(controllerAsText, id, controller.returnMoveList()))
   }
 
   def joinGame(id: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val gameID = id
-    val controller = controllerMapping(gameID)
+    val controller = cleanUpTask.controllerMapping(gameID)
     val controllerAsText: String = controller.output
-    Ok(views.html.chess(controllerAsText, gameID))
+    Ok(views.html.chess(controllerAsText, gameID, controller.returnMoveList()))
+  }
+
+  def adminPage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.admin(cleanUpTask.garbageCollector))
   }
 
 }

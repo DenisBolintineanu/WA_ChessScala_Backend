48d20b7d366761e533d5ee3888a02bca8058ba70
andre-koe <andre.koeniger1997@gmail.com>
2023-08-20 21:12:27 +0200

    Make garbageCollector mutable again

--- app/utils/CleanUpTask.scala
+++ app/utils/CleanUpTask.scala
@@ -4,29 +4,30 @@ import ChessScala.controller.{Controller, IController}
 import akka.actor.ActorSystem
 import com.google.inject.Inject
 
+import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
 
 class CleanUpTask @Inject()(actorSystem: ActorSystem)(implicit executionContext: ExecutionContext) {
 
-  var garbageCollector: Map[String, Boolean] = Map.empty
+  val garbageCollector: mutable.Map[String, Boolean] = mutable.Map.empty
   var controllerMapping: Map[String, IController] = Map.empty
 
-  private val cleanUpIntervalTime = 15.minute
+  private val cleanUpIntervalTime = 7.minute
 
   def gameSetup: String = {
     val gameID = java.util.UUID.randomUUID().toString
     val controller = new Controller()
     controllerMapping = controllerMapping + (gameID -> controller)
-    garbageCollector = garbageCollector + (gameID -> true)
+    garbageCollector.+=(gameID -> true)
     controller.computeInput("1")
     gameID
   }
 
   actorSystem.scheduler.scheduleAtFixedRate(initialDelay = cleanUpIntervalTime, interval = cleanUpIntervalTime) { () =>
     actorSystem.log.info("Executing clean up...")
-    garbageCollector = garbageCollector.filter(x => x._2)
-    garbageCollector = garbageCollector.map(x => (x._1, false))
+    garbageCollector --= garbageCollector.filterNot(x => x._2).keys
+    garbageCollector.foreach(x => garbageCollector(x._1) = false)
     controllerMapping = controllerMapping.filter(x => garbageCollector.contains(x._1))
   }
 }

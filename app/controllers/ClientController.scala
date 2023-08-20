4d4006d14bbf5791e37fdffeed814f1c0ec6dd0c
andre-koe <andre.koeniger1997@gmail.com>
2023-08-20 22:08:40 +0200

    Improve upon post request, remove parameters

--- app/controllers/ClientController.scala
+++ app/controllers/ClientController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import ChessScala.controller.Controller
 import play.api.mvc._
 import utils.CleanUpTask
 
@@ -8,20 +9,33 @@ import javax.inject._
 @Singleton
 class ClientController @Inject()(val controllerComponents: ControllerComponents, val cleanUpTask: CleanUpTask) extends BaseController {
 
+  private val MOVE_ID: String = "move"
+  private val GAME_ID: String = "id"
+  private val INVALID_RESPONSE: String = "invalid"
+  private val ERROR_RESPONSE: String = "ERROR"
+
+
   def createNewGame(): Action[AnyContent] = Action {
     val gameId = cleanUpTask.gameSetup
     Ok(gameId)
   }
 
-  def doMove(id: String, moveAsString: String): Action[AnyContent] = Action {
-    if (!cleanUpTask.controllerMapping.contains(id)) {
-      Ok("ERROR")
-    } else {
-      val controller = cleanUpTask.controllerMapping(id)
-      cleanUpTask.garbageCollector(id) = true
-      controller.computeInput(moveAsString)
-      Ok(controller.returnBoardAsJson())
+  def doMove(): Action[AnyContent] = Action { implicit request: Request[AnyContent] => {
+      val id = returnRequestParamAsString(request, GAME_ID)
+      if (!cleanUpTask.controllerMapping.contains(id)) {
+        Ok(ERROR_RESPONSE)
+      } else {
+        val moveAsString = returnRequestParamAsString(request, MOVE_ID)
+        val controller = cleanUpTask.controllerMapping(id)
+        cleanUpTask.garbageCollector(id) = true
+        controller.computeInput(moveAsString)
+        Ok(controller.returnBoardAsJson())
+      }
     }
+  }
+
+  private def returnRequestParamAsString(request: Request[AnyContent], key: String): String = {
+    request.body.asFormUrlEncoded.get(key).headOption.getOrElse(INVALID_RESPONSE)
   }
 
 }
